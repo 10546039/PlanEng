d802651fcd2272c3951441cf7b3aa72a41225b5e
Ivy Liu <10546028@ntub.edu.tw>
2019-05-28 11:30:31 +0800

    activity_main_button

--- app/src/main/java/com/example/planeng/MainActivity.java
+++ app/src/main/java/com/example/planeng/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         //判斷是否登入
         if (!logon) {
