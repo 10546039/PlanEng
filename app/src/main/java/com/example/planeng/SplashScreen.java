b094ab158e5cea19f319415f9cfffe1c2aec7aba
Ivy Liu <10546028@ntub.edu.tw>
2019-05-28 16:32:18 +0800

    splashscreen

---
+++ app/src/main/java/com/example/planeng/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.planeng;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.planeng.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        //以下撰寫跳轉畫面的程式碼
+        //啟動執行序
+        new Thread(new Runnable() {
+    @Override
+    public void run() {
+        try{
+        Thread.sleep(2000);
+        startActivity(new Intent().setClass(SplashScreen.this, MainActivity.class));
+        }catch (InterruptedException e){
+        e.printStackTrace();
+        }
+        }
+        }).start();
+        }}
