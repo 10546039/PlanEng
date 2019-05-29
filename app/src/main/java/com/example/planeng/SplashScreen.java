59accb45ea944949ac5a29ecedfc55a7365a3842
Ivy Liu <10546028@ntub.edu.tw>
2019-05-29 19:06:05 +0800

    splashscreen-edit

--- app/src/main/java/com/example/planeng/SplashScreen.java
+++ app/src/main/java/com/example/planeng/SplashScreen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.planeng.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
