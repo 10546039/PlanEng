71bdd1754745694384081f71a5d4819090184bdd
10546039 <10546039@ntub.edu.tw>
2019-05-29 20:30:22 +0800

    plan page ui

--- app/src/main/java/com/example/planeng/PlanActivity.java
+++ app/src/main/java/com/example/planeng/PlanActivity.java
@@ -1,6 +1,5 @@
 package com.example.planeng;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,37 +13,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity
+public class PlanActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    //判斷是否登入
-    boolean logon = true;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setContentView(R.layout.activity_plan);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //判斷是否登入
-        if (!logon) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-
-
-
-
-        //FloatingActionButton fab = findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View view) {
-              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-            //}
-        //});
 
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -54,22 +32,6 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-
-
-
-        ImageButton planPageBtn = (ImageButton)findViewById(R.id.b1);
-        planPageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this , PlanActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
     }
 
     @Override
@@ -85,7 +47,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.plan, menu);
         return true;
     }
 
