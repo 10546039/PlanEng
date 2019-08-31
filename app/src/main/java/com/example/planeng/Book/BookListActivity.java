e02c6ff9c8abc520dc67dffafd6b3433030f4bbf
10546052 <10546052@ntub.edu.tw>
2019-08-31 22:11:07 +0800

    update reviewaddpage

--- app/src/main/java/com/example/planeng/Book/BookListActivity.java
+++ app/src/main/java/com/example/planeng/Book/BookListActivity.java
@@ -1,41 +1,32 @@
-package com.example.planeng.Book;
+package com.example.planeng;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.planeng.MainActivity;
-import com.example.planeng.PlanActivity;
-import com.example.planeng.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class BookListActivity extends AppCompatActivity
+public class ReviewActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    private LinearLayout LLinear;
-    List<String> chapNameList;//放置標題的集合
-    List<String> chapFrontList;//放內容的集合
-    String[] iv = {"apple","banana","melon","berry"};
-
-
-
+    private LinearLayout parentLinearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_list);
+        setContentView(R.layout.activity_review);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -46,68 +37,35 @@ public class BookListActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-        initView();
-        initData();
-
-        ImageButton addBook_bt = (ImageButton)findViewById(R.id.addbook_bt);
-        addBook_bt.setOnClickListener(new View.OnClickListener() {
+        ImageButton reWBtn = (ImageButton)findViewById(R.id.imageButton12);
+        ImageButton noteaddPageBtn = (ImageButton)findViewById(R.id.imageButton13);
+        parentLinearLayout = (LinearLayout)findViewById(R.id.parentLinearLayout);
+        noteaddPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(BookListActivity.this , BookSetActivity.class);
+                intent.setClass(ReviewActivity.this , Review_add_Activity.class);
+                startActivity(intent);
+
+                ImageButton button = new ImageButton(getApplicationContext());
+                button.setImageResource(R.drawable.review_more);
+                button.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+                ViewGroup.LayoutParams layoutBB = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                button.setLayoutParams(layoutBB);
+                // parentLinearLayout.addView(imageview);
+                parentLinearLayout.addView(button);
+            }
+        });
+        reWBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(ReviewActivity.this , Review_add_Activity.class);
                 startActivity(intent);
             }
         });
-    }
-
-
-    private void initView() {
-        //要新增view的容器
-        LLinear = findViewById(R.id.ListLinear);
-        chapNameList = new ArrayList<>();
-        chapFrontList = new ArrayList<>();
-
-    }
-    /**
-     * 處理資料,可以是伺服器請求過來的,也可以是本地的
-     */
-    private void initData() {
-        for (int i = 0; i < iv.length; i++) {
-            chapFrontList.add(iv[i]);
-            chapNameList.add("第" + (i+1) + "本");
-        }
-        //資料拿到之後去根據資料去動態新增View
-        addView();
-    }
-
-    /**
-     * 動態新增的具體實現
-     */
-
-    TextView bookName;
-    TextView chapFront;
-
-    private void addView() {
-        //ivList集合有幾個元素就新增幾個
-        for (int i = 0; i < chapFrontList.size(); i++) {
-            //首先引入要新增的View
-            View Listview = View.inflate(this, R.layout.book, null);
-            //找到裡面需要動態改變的控制元件
-            bookName = Listview.findViewById(R.id.bookName);
-            chapFront = Listview.findViewById(R.id.chapFront);
-            //給控制元件賦值
-            bookName.setText(chapNameList.get(i));
-            chapFront.setText(chapFrontList.get(i));
-
-
-            //把所有動態建立的view都新增到容器裡面
-            LLinear.addView(Listview);
-        }
-
-    }
-
-
-
+            }
 
     @Override
     public void onBackPressed() {
@@ -122,7 +80,7 @@ public class BookListActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.book_list, menu);
+        getMenuInflater().inflate(R.menu.review, menu);
         return true;
     }
 
@@ -147,23 +105,17 @@ public class BookListActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-
-
         if (id == R.id.nav_home) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_book) {
-            Intent intent = new Intent(this, BookListActivity.class);
-            startActivity(intent);
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_note) {
+        } else if (id == R.id.nav_tools) {
 
-        } else if (id == R.id.nav_review) {
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_plan) {
-            Intent intent = new Intent(this, PlanActivity.class);
-            startActivity(intent);
+        } else if (id == R.id.nav_send) {
 
         }
 
