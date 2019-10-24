88802d7e9b5b279e4342111dc5df85065e3a33d2
10546039 <10546039@ntub.edu.tw>
2019-10-24 16:30:22 +0800

    讀書計畫連資料庫並顯示

--- app/src/main/java/com/example/planeng/getTask.java
+++ app/src/main/java/com/example/planeng/getTask.java
@@ -12,10 +12,11 @@ public class getTask extends StringRequest {
     private static final String GETTASK_URL = "https://108401.000webhostapp.com/getTask.php";
     private Map<String, String> params;
 
-    public getTask(String dbDate, Response.Listener<String> listener) {
+    public getTask(String m_id, String date, Response.Listener<String> listener) {
         super(Method.POST, GETTASK_URL, listener, null);
         params = new HashMap<>();
-        params.put("date", dbDate);
+        params.put("m_id", m_id);
+        params.put("date", date);
 
     }
 
