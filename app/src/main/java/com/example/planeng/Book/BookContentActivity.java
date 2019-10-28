ccf6aac6c3b416ffd2ebc14d92427fd9007315f7
10546052 <10546052@ntub.edu.tw>
2019-10-28 13:20:39 +0800

    重創分支52(2)

--- app/src/main/java/com/example/planeng/Book/BookContentActivity.java
+++ app/src/main/java/com/example/planeng/Book/BookContentActivity.java
@@ -1,48 +1,27 @@
-package com.example.planeng;
+package com.example.planeng.Book;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.example.planeng.MainActivity;
+import com.example.planeng.PlanActivity;
+import com.example.planeng.R;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Review_add_Activity extends AppCompatActivity
+public class BookContentActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    EditText etReview;
-    ImageButton bReview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review_add_);
+        setContentView(R.layout.activity_book_content);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -52,137 +31,7 @@ public class Review_add_Activity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        etReview = findViewById(R.id.reviewdata);
-
-
-
-
-        ImageButton saveBook = findViewById(R.id.savereview);
-        saveBook.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final EditText data = findViewById(R.id.reviewdata);
-                if (data.equals("")){
-                    showDialog(datacheck);
-                }else{
-                    showDialog(check);
-                }
-            }
-        });
-
-
     }
-    final int check = 1000;
-    final int datacheck = 2000;
-    @Override
-    protected Dialog onCreateDialog(int id)  //初始化對話方塊，透過 showDialog(ID) 觸發
-    {
-        Dialog dialog = null;
-
-        switch(id) //判斷所傳入的ID，啟動相應的對話方塊
-        {
-            case check:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("儲存心得") //設定標題文字
-                        .setMessage("確認發表") //設定內容文字
-                        .setPositiveButton("確認", new DialogInterface.OnClickListener()
-                        { //設定確定按鈕
-                            @Override
-                            public void onClick(DialogInterface dialog, int which)
-                            {
-                                // TODO Auto-generated method stub
-
-                                send();
-
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener()
-                        { //設定取消按鈕
-                            @Override
-                            public void onClick(DialogInterface dialog, int which)
-                            {
-                                // TODO Auto-generated method stub
-                            }
-                        });
-
-                dialog = builder.create(); //建立對話方塊並存成 dialog
-                break;
-
-            case datacheck:
-                AlertDialog.Builder changeBuilder = new AlertDialog.Builder(this);
-                dialog = changeBuilder.create(); //建立對話方塊並存成 dialog
-                break;
-
-            default:
-                break;
-        }
-        return dialog;
-    }
-
-    private void send() {
-        String r_id = "22";
-        String review =etReview.getText().toString();
-
-                Response.Listener<String> responseListener1 = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success) {
-
-                                Intent intent1 = new Intent(Review_add_Activity.this, ReviewActivity.class);
-                                Review_add_Activity.this.startActivity(intent1);
-                            } else {
-                                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(Review_add_Activity.this);
-                                builder.setMessage("Register Failed")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-
-                Reviewadd save = new Reviewadd(r_id, review, responseListener1);
-                RequestQueue queue1 = Volley.newRequestQueue(Review_add_Activity.this);
-                queue1.add(save);
-
-
-
-
-
-
-
-        Intent intent = new Intent(this, Review_add_Activity.class);
-        startActivity(intent);
-        Review_add_Activity.this.finish();
-        Toast.makeText(Review_add_Activity.this,"新增成功！", Toast.LENGTH_LONG).show();
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public void onBackPressed() {
@@ -197,7 +46,7 @@ public class Review_add_Activity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.review_add_, menu);
+        getMenuInflater().inflate(R.menu.book_content, menu);
         return true;
     }
 
@@ -222,17 +71,23 @@ public class Review_add_Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+
+
         if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_book) {
+            Intent intent = new Intent(this, BookListActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_note) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_review) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_plan) {
+            Intent intent = new Intent(this, PlanActivity.class);
+            startActivity(intent);
 
         }
 
@@ -240,5 +95,4 @@ public class Review_add_Activity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 }
--- app/src/main/java/com/example/planeng/Book/BookListActivity.java
+++ app/src/main/java/com/example/planeng/Book/BookListActivity.java
@@ -1,41 +1,41 @@
-package com.example.planeng;
+package com.example.planeng.Book;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.ViewGroup;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.example.planeng.MainActivity;
+import com.example.planeng.PlanActivity;
+import com.example.planeng.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReviewActivity extends AppCompatActivity
+public class BookListActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private LinearLayout parentLinearLayout;
+
+    private LinearLayout LLinear;
+    List<String> chapNameList;//放置標題的集合
+    List<String> chapFrontList;//放內容的集合
+    String[] iv = {"apple","banana","melon","berry"};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //adddddddddddddd
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        setContentView(R.layout.activity_book_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -46,69 +46,65 @@ public class ReviewActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
+        initView();
+        initData();
 
-        ImageButton noteaddPageBtn = (ImageButton)findViewById(R.id.imageButton13);
-        parentLinearLayout = (LinearLayout)findViewById(R.id.parentLinearLayout);
-        noteaddPageBtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton addBook_bt = (ImageButton)findViewById(R.id.addbook_bt);
+        addBook_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(ReviewActivity.this , Review_add_Activity.class);
+                intent.setClass(BookListActivity.this , BookSetActivity.class);
                 startActivity(intent);
             }
         });
-        initView();
+    }
 
+
+    private void initView() {
+        //要新增view的容器
+        LLinear = findViewById(R.id.ListLinear);
+        chapNameList = new ArrayList<>();
+        chapFrontList = new ArrayList<>();
+
+    }
+    /**
+     * 處理資料,可以是伺服器請求過來的,也可以是本地的
+     */
+    private void initData() {
+        for (int i = 0; i < iv.length; i++) {
+            chapFrontList.add(iv[i]);
+            chapNameList.add("第" + (i+1) + "本");
+        }
+        //資料拿到之後去根據資料去動態新增View
         addView();
     }
 
+    /**
+     * 動態新增的具體實現
+     */
 
-
-    private LinearLayout mLinear;
-    private List<String> textid;
-    private void initView() {
-        //要新增view的容器
-        mLinear = findViewById(R.id.parentLinearLayout);
-        textid =new ArrayList<>();
-
-    }
-
-
-    TextView rt;
-    ImageButton reviewbt;
+    TextView bookName;
+    TextView chapFront;
 
     private void addView() {
         //ivList集合有幾個元素就新增幾個
+        for (int i = 0; i < chapFrontList.size(); i++) {
+            //首先引入要新增的View
+            View Listview = View.inflate(this, R.layout.book, null);
+            //找到裡面需要動態改變的控制元件
+            bookName = Listview.findViewById(R.id.bookName);
+            chapFront = Listview.findViewById(R.id.chapFront);
+            //給控制元件賦值
+            bookName.setText(chapNameList.get(i));
+            chapFront.setText(chapFrontList.get(i));
 
-        for(int i=0;i<9;i++) {
-            View view = View.inflate(this, R.layout.reviewdp, null);
-            rt = view.findViewById(R.id.reveiwID);
-            reviewbt = view.findViewById(R.id.morerbt);
-            textid.add("第" + i + "個");
-            rt.setText(textid.get(i));
-            final int finalI = i;//由於OnClick裡面拿不到i,所以需要重新賦值給一個final物件
-            reviewbt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(ReviewActivity.this, "點選了"+textid.get(finalI), Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent();
-                    intent.setClass(ReviewActivity.this ,Review_add_Activity.class);
-                    startActivity(intent);
-                }
-            });
 
-            mLinear.addView(view);
+            //把所有動態建立的view都新增到容器裡面
+            LLinear.addView(Listview);
         }
+
     }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -126,7 +122,7 @@ public class ReviewActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.review, menu);
+        getMenuInflater().inflate(R.menu.book_list, menu);
         return true;
     }
 
@@ -151,17 +147,23 @@ public class ReviewActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+
+
         if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_book) {
+            Intent intent = new Intent(this, BookListActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_note) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_review) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_plan) {
+            Intent intent = new Intent(this, PlanActivity.class);
+            startActivity(intent);
 
         }
 
